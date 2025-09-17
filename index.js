2849d4fb3b814b2c5e2c3ad5467538e7b413ee95
LucianoBuhler <lucianobuhler@gmail.com>
2025-09-17 15:45:54 -0300

    feat: Added Trails schemas and resolvers

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { gql } from "graphql-tag";
 import lifts from "./data/lifts.json" with { type: "json" };
-// import trails from "./data/trails.json" with { type: "json" };
+import trails from "./data/trails.json" with { type: "json" };
 
 const typeDefs = gql`
   type Lift {
@@ -18,10 +18,34 @@ const typeDefs = gql`
     CLOSED
     HOLD
   }
+
+  type Trail {
+    id: ID!
+    name: String!
+    difficulty: TrailDifficulty!
+    status: TrailStatus!
+    grromed: Boolean!
+    snowmaking: Boolean!
+    trees: Boolean!
+    night: Boolean!
+  }
+  enum TrailStatus {
+    OPEN
+    CLOSED
+  }
+  enum TrailDifficulty {
+    beginner
+    intermediate
+    advanced
+    expert
+  }
   type Query {
     liftCount: Int!
     allLifts: [Lift!]!
     findLiftById(id: ID!): Lift!
+    trailCount: Int!
+    allTrails: [Trail!]!
+    findTrailById(id: ID!): Trail!
   }
 `;
 
@@ -29,7 +53,12 @@ const resolvers = {
   Query: {
     liftCount: () => lifts.length,
     allLifts: () => lifts,
-    findLiftById: (parent, args) => lifts.find((lift) => lift.id === args.id)
+    findLiftById: (parent, args) => 
+      lifts.find((lift) => lift.id === args.id),
+    trailCount: () => trails.length,
+    allTrails: () => trails,
+    findTrailById: (parent, args) => 
+      trails.find((trail) => trail.id === args.id)
   }
 };
 
